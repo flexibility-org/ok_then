b61ac181ffe942fb9d2844e3d195761d023d6f42
Paul Dann <paul.dann@flexibility.ai>
2021-05-25 12:56:38 +0100

    [Mix] Reduce requirement to Elixir 1.6

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule OkThen.MixProject do
       app: :ok_then,
       version: "0.1.0",
       description: description(),
-      elixir: "~> 1.11",
+      elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       docs: [
