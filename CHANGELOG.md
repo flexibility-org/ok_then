ecd37d6a47359693699ff10a81d7fd9a830f1cb6
Paul Dann <paul.dann@flexibility.ai>
2021-05-31 11:28:53 +0100

    Tag 0.1.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-### Unreleased
+### 0.1.1
 
 * Made some argument names and typespecs more consistent.
 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule OkThen.MixProject do
   def project do
     [
       app: :ok_then,
-      version: "0.1.0",
+      version: "0.1.1",
       description: description(),
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
